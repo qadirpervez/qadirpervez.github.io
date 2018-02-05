5b633f6d3db40e8d293010534c26a732c69952da
Qadir Pervez <786.qadir.pervez@gmail.com>
2018-02-06 01:05:00 +0530

    check offc

--- localWeather/app.js
+++ localWeather/app.js
@@ -1,1 +1,5 @@
-//drizzle, clouds, rain, snow, clear, thunderstorm, Mist https://github.com/freeCodeCamp/freeCodeCamp/issues/15767
+//drizzle, clouds, rain, snow, clear, thunderstorm, Mist
+ https://github.com/freeCodeCamp/freeCodeCamp/issues/15767
+
+https://openweathermap.org/weather-conditions
+
