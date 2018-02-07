f05112c1c451388f3f2e8bbdca4cc9286b30d9be
Qadir Pervez <786.qadir.pervez@gmail.com>
2018-02-08 02:03:34 +0530

    message getting need a lot changes

--- localWeather/app.js
+++ localWeather/app.js
@@ -15,7 +15,7 @@ var apiOBJ = {
   url: "https://api.openweathermap.org/data/2.5/forecast?",
   // q=London&appid=b6907d289e10d714a6e88b30761fae22 By CityName
   // lat=35&lon=139 By Lat and Long
-  apiKey: "7336c358d9a2fe6239a7f49f942f8688" // Success Responce: cod == 200, ERROR RES: cod: 404 OR 400,
+  apiKey: "&appid=7336c358d9a2fe6239a7f49f942f8688" // Success Responce: cod == 200, ERROR RES: cod: 404 OR 400,
 };
 var images = {
   clearW: "images/clear.jpeg",
@@ -88,13 +88,103 @@ window.getGeo = function(){
       $("#locate").hide();
       $("#weatherFetch").show();
     }
-    setTimeout(window.getWeatherData, 2000);
+    setTimeout(window.getWeatherDataGeo, 2000);
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    window.locateFail(errorThrown);
   });
 }
-window.getWeatherData = function(){
-  var url = window.apiOBJ.url+"lat="+window.lat+"&lon="+window.long+"&appid="+window.apiOBJ.apiKey;
+window.getWeatherDataGeo = function(){
+  var url = window.apiOBJ.url+"lat="+window.lat+"&lon="+window.long + window.apiOBJ.apiKey;
   $.getJSON(url, function ( data ){
-    console.log(data);
+    window.weatherDataFirst = data;
+    $("#weatherFetch").hide();
+    window.showWeatherData(data);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    //invalid coord, response 401, 400, 404 every error response.
+    //edit this later,
   });
 }
+
+window.getWeatherDataGeoCity = function(queryString){
+  var url = window.apiOBJ.url+"q="+queryString+window.apiOBJ.apiKey;
+  $.getJSON(url, function ( data ){
+      console.log(data)
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    //Not found city response 401, 400, 404 every error response.
+  });
+}
+window.showWeatherData = function(weather){
+  var city = window.locObj.city || weather.city.name;
+  var state = window.locObj.region_code;
+  var country = window.locObj.country_code || weather.city.country;
+  var stHtml = ''; // storing html if ther is state.
+  var headingText = '';
+  if(state !== '' ){
+    stHtml = ', '+state;
+  } else {
+    country = window.locObj.country_name || window.locObj.country_code || weather.city.country;
+  }
+  headingText = city+stHtml+', '+country;
+  $("#geoPlace").html(headingText);
+
+  var type = weather.list[0].weather[0].main;
+  var desc = weather.list[0].weather[0].description;
+  temp = parseInt(weather.list[0].main.temp);
+  window.tempC = temp - 273;
+  window.tempF = Math.round(((tempC * 9) / 5) + 32);
+  window.tempC = Math.round(window.tempC);
+
+  switch(type){
+    case "Clouds":
+        window.changeBg('clouds');
+        break;
+    case "Rain":
+        window.changeBg('rain');
+        break;
+    case "Snow":
+        window.changeBg('snow');
+        break;
+    case "Clear":
+        window.changeBg('clear');
+        break;
+    case "Thunderstorm":
+        window.changeBg('thunder');
+        break;
+    default:
+        window.changeBg('');
+  }
+
+  $("#temp").html(window.tempC);
+  $("#temperature").show();
+}
+window.changeBg = function(str){
+  switch(str){
+    case 'clouds':
+      $("#bg").attr("class", "cloudsA");
+      $("#skyCloudData").html(window.cloudsHTML);
+      break;
+    case 'rain':
+      $("#bg").attr("class", "rainA");
+      $("#skyCloudData").html(window.rainHTML);
+      break;
+    case 'snow':
+      $("#bg").attr("class", "snowA");
+      $("#skyCloudData").html(window.snowHTML);
+      break;
+    case 'clear':
+      $("#bg").attr("class", "clearA");
+      $("#skyCloudData").html(window.clearWHTML);
+      break;
+    case 'thunder':
+      $("#bg").attr("class", "thunderA");
+      $("#skyCloudData").html(window.thunderstormHTML);
+      break;
+    default:
+      $("#bg").attr("class", "");
+  }
+}
+
 setTimeout(getGeo, 2000);
+window.locateFail = function(error){
+  console.log('Failed getting the location', error);
+}
