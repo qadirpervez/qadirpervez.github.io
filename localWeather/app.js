8c1575f7e7b31fe1d6d6ebceb1ea66f3f717033a
Qadir Pervez <786.qadir.pervez@gmail.com>
2018-02-09 01:26:29 +0530

    50% Done

--- localWeather/app.js
+++ localWeather/app.js
@@ -14,7 +14,7 @@ var apiOBJ = {
   locate: "https://freegeoip.net/json/", //for location as browser has some issue with my coding.
   url: "https://api.openweathermap.org/data/2.5/forecast?",
   // q=London&appid=b6907d289e10d714a6e88b30761fae22 By CityName
-  // lat=35&lon=139 By Lat and Long
+  // lat=35&lon=139cnt=10 total 10 days By Lat and Long
   apiKey: "&appid=7336c358d9a2fe6239a7f49f942f8688" // Success Responce: cod == 200, ERROR RES: cod: 404 OR 400,
 };
 var images = {
@@ -75,7 +75,7 @@ snowHTML = window.snow,
 clearWHTML = window.clearW,
 rainHTML = window.rain;
 window.iconsMainDiv.style.display = "none";
-var lat, long, city, locObj;
+var lat, long, city, locObj, usedLocation = false;
 
 window.getGeo = function(){
   $.getJSON(window.apiOBJ.locate, function ( data ){
@@ -93,8 +93,10 @@ window.getGeo = function(){
     window.locateFail(errorThrown);
   });
 }
-window.getWeatherDataGeo = function(){
-  var url = window.apiOBJ.url+"lat="+window.lat+"&lon="+window.long + window.apiOBJ.apiKey;
+window.getWeatherDataGeo = function(lat, lon){
+  lat = lat || window.lat;
+  lon = lon || window.long;
+  var url = window.apiOBJ.url+"lat="+lat+"&lon="+lon + window.apiOBJ.apiKey;
   $.getJSON(url, function ( data ){
     window.weatherDataFirst = data;
     $("#weatherFetch").hide();
@@ -108,27 +110,29 @@ window.getWeatherDataGeo = function(){
 window.getWeatherDataGeoCity = function(queryString){
   var url = window.apiOBJ.url+"q="+queryString+window.apiOBJ.apiKey;
   $.getJSON(url, function ( data ){
-      console.log(data)
+    console.log(data)
   }).fail(function(jqXHR, textStatus, errorThrown){
     //Not found city response 401, 400, 404 every error response.
   });
 }
 window.showWeatherData = function(weather){
-  var city = window.locObj.city || weather.city.name;
-  var state = window.locObj.region_code;
-  var country = window.locObj.country_code || weather.city.country;
-  var stHtml = ''; // storing html if ther is state.
+  var city = weather.city.name;
+  var country = weather.city.country;
   var headingText = '';
-  if(state !== '' ){
-    stHtml = ', '+state;
-  } else {
-    country = window.locObj.country_name || window.locObj.country_code || weather.city.country;
-  }
-  headingText = city+stHtml+', '+country;
+  headingText = city+', '+country;
   $("#geoPlace").html(headingText);
 
   var type = weather.list[0].weather[0].main;
-  var desc = weather.list[0].weather[0].description;
+  $("#desc").html(weather.list[0].weather[0].description);
+  $("#humidity").html(weather.list[0].main.humidity + ' %');
+  $("#pressure").html(weather.list[0].main.grnd_level + ' hPa');
+  $("#windDesc").html(weather.list[0].wind.speed + ' m / s');
+  $("#otherDetails").show();
+  if(window.usedLocation === false){
+    $("#usedLoc").show();
+  } else {
+    $("#usedLoc").hide();
+  }
   temp = parseInt(weather.list[0].main.temp);
   window.tempC = temp - 273;
   window.tempF = Math.round(((tempC * 9) / 5) + 32);
@@ -136,22 +140,22 @@ window.showWeatherData = function(weather){
 
   switch(type){
     case "Clouds":
-        window.changeBg('clouds');
-        break;
+    window.changeBg('clouds');
+    break;
     case "Rain":
-        window.changeBg('rain');
-        break;
+    window.changeBg('rain');
+    break;
     case "Snow":
-        window.changeBg('snow');
-        break;
+    window.changeBg('snow');
+    break;
     case "Clear":
-        window.changeBg('clear');
-        break;
+    window.changeBg('clear');
+    break;
     case "Thunderstorm":
-        window.changeBg('thunder');
-        break;
+    window.changeBg('thunder');
+    break;
     default:
-        window.changeBg('');
+    window.changeBg('');
   }
 
   $("#temp").html(window.tempC);
@@ -160,30 +164,53 @@ window.showWeatherData = function(weather){
 window.changeBg = function(str){
   switch(str){
     case 'clouds':
-      $("#bg").attr("class", "cloudsA");
-      $("#skyCloudData").html(window.cloudsHTML);
-      break;
+    $("#bg").attr("class", "cloudsA");
+    $("#skyCloudData").html(window.cloudsHTML);
+    break;
     case 'rain':
-      $("#bg").attr("class", "rainA");
-      $("#skyCloudData").html(window.rainHTML);
-      break;
+    $("#bg").attr("class", "rainA");
+    $("#skyCloudData").html(window.rainHTML);
+    break;
     case 'snow':
-      $("#bg").attr("class", "snowA");
-      $("#skyCloudData").html(window.snowHTML);
-      break;
+    $("#bg").attr("class", "snowA");
+    $("#skyCloudData").html(window.snowHTML);
+    break;
     case 'clear':
-      $("#bg").attr("class", "clearA");
-      $("#skyCloudData").html(window.clearWHTML);
-      break;
+    $("#bg").attr("class", "clearA");
+    $("#skyCloudData").html(window.clearWHTML);
+    break;
     case 'thunder':
-      $("#bg").attr("class", "thunderA");
-      $("#skyCloudData").html(window.thunderstormHTML);
-      break;
+    $("#bg").attr("class", "thunderA");
+    $("#skyCloudData").html(window.thunderstormHTML);
+    break;
     default:
-      $("#bg").attr("class", "");
+    $("#bg").attr("class", "");
   }
 }
-
+window.showCelcius = function (){
+  $("#temp").html(window.tempC);
+  $("#celcius").addClass("activeUnit");
+  $("#farenhiet").removeClass("activeUnit");
+}
+window.showFarenhiet = function (){
+  $("#temp").html(window.tempF);
+  $("#celcius").removeClass("activeUnit");
+  $("#farenhiet").addClass("activeUnit");
+}
+window.getLocCallApi = function (){
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      window.usedLocation = true;
+      window.lat = position.coords.latitude;
+      window.long = position.coords.longitude;
+      window.getWeatherDataGeo(window.lat, window.long)
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    console.log('Not Supported geo location');
+  }
+}
 setTimeout(getGeo, 2000);
 window.locateFail = function(error){
   console.log('Failed getting the location', error);
