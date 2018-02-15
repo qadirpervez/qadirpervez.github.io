14719f0a19271fbe156393b3cd31a6472caafb75
Qadir Pervez <786.qadir.pervez@gmail.com>
2018-02-15 22:23:10 +0530

    Quote Machine

---
+++ quoteMachine/app.js
@@ -0,0 +1,53 @@
+/*  Javascript Code for Random Quote Machine Game;
+made for freeCodeCamp.
+
+Owner: Qadir Pervez
+*/
+var url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+function getQuote(){
+  $.ajax({
+
+    // The 'type' property sets the HTTP method.
+    // A value of 'PUT' or 'DELETE' will trigger a preflight request.
+    type: 'GET',
+
+    // The URL to make the request to.
+    url: url,
+
+    // The 'contentType' property sets the 'Content-Type' header.
+    // The JQuery default for this property is
+    // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
+    // a preflight. If you set this value to anything other than
+    // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
+    // you will trigger a preflight request.
+    contentType: 'text/plain',
+
+    xhrFields: {
+      // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+      // This can be used to set the 'withCredentials' property.
+      // Set the value to 'true' if you'd like to pass cookies to the server.
+      // If this is enabled, your server must respond with the header
+      // 'Access-Control-Allow-Credentials: true'.
+      withCredentials: false
+    },
+
+    headers: {
+      // Set any custom headers here.
+      // If you set any non-simple headers, your server must include these
+      // headers in the 'Access-Control-Allow-Headers' response header.
+    },
+
+    success: function(data) {
+      // Here's where you handle a successful response.
+      console.log(data);
+    },
+
+    error: function() {
+      // Here's where you handle an error response.
+      // Note that if the error was due to a CORS issue,
+      // this function will still fire, but there won't be any additional
+      // information about the error.
+    }
+  });
+}
+getQuote();
