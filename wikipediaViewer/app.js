84ae93bf33f04c74195b589aa8ef861e95028fb9
Qadir Pervez <786.qadir.pervez@gmail.com>
2018-02-18 18:18:19 +0530

    done wikipedia viewer

--- wikipediaViewer/app.js
+++ wikipediaViewer/app.js
@@ -5,7 +5,8 @@ developed by Qadir Pervez
 function checkBlank(){
   searchText = $("#searchText").val();
   if(searchText == ''){
-    $("#wikiData").html('');
+    $("#wikiData").fadeOut(300);
+    // $("#wikiData").html('');
   } else if(searchText.length > 5){
     searchWiki();
   }
@@ -13,9 +14,7 @@ function checkBlank(){
 window.url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
 link = "https://en.wikipedia.org/?curid=";
 function searchWiki(){
-  console.log("done");
   searchText = $("#searchText").val();
-  console.log(searchText);
   if(searchText != ''){
     wikiApi = url + searchText;
     $.getJSON(wikiApi, function ( data ){
@@ -36,28 +35,22 @@ function searchWiki(){
 
       for (var k in pages){
         if (pages.hasOwnProperty(k)) {
-          console.log(k);
           let pageUrl = link + pages[k].pageid;
-          let html = `<a class="col-md-12" target="_blank" href="`+pageUrl+`">
+          let html = `<a class="col-lg-12 col-md-12 col-sm-12 col-xs-12 animation" target="_blank" href="`+pageUrl+`">
           <div>
           <h1>`+pages[k].title+`</h1>
           <p>`+pages[k].extract+`</p>
           </div>
           </a>`;
           $("#wikiData").append(html);
-
+          $("#wikiData").show();
         }
       }
-      // for(i = 0; i < 11; i++){
-      //   let pageUrl = link + data.query.pages[i].pageid;
-      //   let html = `<a class="col-md-12" href="`+pageUrl+`">
-      //   <div>
-      //   <h1>`+data.query.pages[i].title+`</h1>
-      //   <p>`+data.query.pages[i].extract+`</p>
-      //   </div>
-      //   </a>`;
-      //   $("#wikiData").append(html);
-      // }
     });
   }
 }
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        searchWiki();
+    }
+});
